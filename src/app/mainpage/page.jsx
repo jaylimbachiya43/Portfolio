5ca5a9dda69a79d1e5ab8e366b5d251c92a2cf69
jaylimbachiya43 <109168679+jaylimbachiya43@users.noreply.github.com>
2023-06-09 10:13:05 +0530

    first

---
+++ src/app/mainpage/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/navbar'
+import React from 'react'
+
+const Mainpage = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Mainpage
